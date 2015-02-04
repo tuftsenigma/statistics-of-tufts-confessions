01bb8c50d7ab0763ebd7f0c2e3426f17bb9e9f03
Soubhik Barari <soubhikbarari@gmail.com>
2015-02-04 01:21:12 -0500

    did a shit ton of topic modelling

--- confessions.py
+++ confessions.py
@@ -29,8 +29,8 @@ Scripts used for 'TuftsConfessions' analysis
 
 DATA TO GET:
 1. popular words -- top trigrams vs specific Tufts trigrams x
-2. sentiment score vs frequency vis -- need to actually write sentiment score to db, b/c throttled over time.. redo
-3. LDA into good topics and map against popularity
+2. sentiment score vs frequency vis -- need to actually write sentiment score to db, b/c throttled over time.. redo and label scores accordingly
+3. LDA into good topics and map against popularity - look through different shaped models 2/2 and 2/3 and cherry pick
 4. daily, monthly trends
 5. correlation between sentiment and likes (?)
 
@@ -46,15 +46,18 @@ dbname 	= 'tufts_trends'
 
 DB = DBCloudStore(host=dbhost, port=dbport, db=dbname, usr=dbusr, psswd=dbpsswd) 
 
-def model_topics(docs, terms):
+def model_topics(docs, terms, num_topics):
 	'''Run Latent Dirichlet Allocation on docs x terms'''
-	NUM_TOPICS = 10
-	NUM_WORDS  = 16
+	if num_topics:
+		NUM_TOPICS = num_topics
+	else:
+		NUM_TOPICS = 15
+	NUM_WORDS  = 15
 	dictionary = corpora.Dictionary(docs) # map words to ids
 	corpus = [dictionary.doc2bow(d) for d in docs] # create sparse vector of doc word-counts
 
 	print "<preparing to LDA>"
-	lda = gensim.models.ldamodel.LdaModel(corpus, id2word=dictionary, num_topics=NUM_TOPICS, update_every=1, passes=25)
+	lda = gensim.models.ldamodel.LdaModel(corpus, id2word=dictionary, num_topics=NUM_TOPICS, update_every=1, passes=50)
 
 	print "<formed LDA model>"
 	topics = lda.show_topics(num_topics=NUM_TOPICS, num_words=NUM_WORDS, log=True, formatted=False)
@@ -130,6 +133,7 @@ def popular_bigrams(docs):
 	finder.apply_freq_filter(50)
 	bigrams = finder.nbest(bigram_measures.pmi, 50)
 	bi_tokens = nltk.bigrams(text)
+
 	print "<printing bigram freqs>"
 	for bi in bigrams:
 		score = finder.score_ngram(bigram_measures.pmi, bi[0], bi[1])
@@ -146,6 +150,7 @@ def popular_trigrams(docs):
 	finder.apply_freq_filter(500)
 	trigrams = finder.nbest(trigram_measures.pmi, 50)
 	tri_tokens = nltk.trigrams(text)
+
 	print "<printing trigram freqs>"
 	for tri in trigrams:
 		score = finder.score_ngram(trigram_measures.pmi, tri[0], tri[1], tri[2])
@@ -168,11 +173,15 @@ def sentiment_analysis(cllxn):
 		count += 1
 		print "collected {} documents".format(count)
 		try:
-			if not post["sentiment"]:
+			print post["sentiment"]
+		except KeyError:
+			try:
 				doc = (post["message"].encode('utf-8'), post["id"])
 				docs.append(doc)
-		except KeyError:
-			continue
+			except KeyError:
+				continue
+
+	time.sleep(10)
 
 	count = 0
 	for doc in docs:
@@ -180,12 +189,15 @@ def sentiment_analysis(cllxn):
 		print "analyzed {} documents".format(count)
 		pt = get_sentiment(doc[0])
 		if pt:
-			DB.update(cllxn, "sentiment", round(pt, 1))
+			print pt
+			DB.update(cllxn, {"id" : doc[1]}, { "$set" : { "sentiment" : round(pt, 1) }})
 			data[round(pt, 1)] += 1
+			print DB.find(cllxn, { "id" : doc[1]})
 
 	slogger.info("total words={}".format(len(list(vocab))))
 	slogger.info("total docs={}".format(len(docs)))
 	slogger.info(data)
+
 	print data
 
 
@@ -227,6 +239,7 @@ if __name__ == '__main__':
 		if e == "popular_trigrams":
 			popular_trigrams(texts)
 		if e == "model_topics":
+			topics = [7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28]
 			cache_path = path.relpath('cache/{}.pkl'.format(cllxn))
 			with open(cache_path, 'rb') as cache:
 				# try to quickly load from cached file
@@ -245,7 +258,10 @@ if __name__ == '__main__':
 				with open(cache_path, 'wb') as cache:
 					pickle.dump((docs, vocab), cache)
 
-			model_topics(docs, vocab)
+			for t in topics:
+				slogger.info("{} TOPIC MODEL".format(t))
+				model_topics(docs, vocab, t)
+
 		if e == "sentiment_analysis":
 			sentiment_analysis(cllxn)
 
